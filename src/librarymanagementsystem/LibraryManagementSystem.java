74b806267d022d97112df4b6faaef6cc0fc156b0
Mamoun1 <diabmam@sheridancollege.ca>
2020-02-10 10:35:02 -0500

    updated

--- src/librarymanagementsystem/LibraryManagementSystem.java
+++ src/librarymanagementsystem/LibraryManagementSystem.java
@@ -17,7 +17,7 @@ public class LibraryManagementSystem {
     public static void main(String[] args) {
        
         System.out.println("Hello");
-        //new line
+        //new line please
     }
     
 }
